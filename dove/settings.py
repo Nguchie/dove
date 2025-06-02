225e87afc523f20f6563c66b32c1188b8c938597
Nguchie. A <110086339+Nguchie@users.noreply.github.com>
2025-06-03 00:55:41 +0300

    Update settings.py

--- dove/settings.py
+++ dove/settings.py
@@ -148,11 +148,4 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-#
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'mail.stackmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'info@doverealestateltd.com'
-# EMAIL_HOST_PASSWORD = 'a-^f@5ziW\-r'
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
